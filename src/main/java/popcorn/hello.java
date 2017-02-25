113e188a73c67b23be583ba1a077b4d16555281a
Prathyush-Gopal <mprathyush1991@gmail.com>
2017-02-25 20:09:45 +0530

    hello.java added

---
+++ src/main/java/popcorn/hello.java
@@ -0,0 +1,13 @@
+package popcorn;
+
+public class hello {
+	/*this is to 
+	 * print the message
+	 */
+	public static void main(String[] args){
+		System.out.println("hello"); 
+		System.out.println("new line added");
+		
+	}
+
+}
